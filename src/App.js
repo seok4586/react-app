bc87c1b513880de88884760743287db503e70f92
wandol <seok45861@gmail.com>
2019-09-22 16:17:07 +0900

    first
    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class TOC extends Component {
+	render() {	
+  		return (
+  			<nav>
+				<ul>
+				<li><a href="1.html">html</a></li>
+				<li><a href="2.html">css</a></li>
+				<li><a href="3.html">javascript</a></li>
+
+				</ul>		
+			</nav>
+  		);
+  	}
+}
+
+class Contents extends Component {
+	render() {	
+  		return (
+  			<article>
+				<h2>{this.props.title}</h2>
+				{this.props.sub}
+			</article>
+  		);
+  	}
+}
+
+class Subject extends Component {
+	render () {
+		return (
+			<header>
+				<h1>{this.props.title}</h1>
+				{this.props.sub}	
+			</header>
+		);
+	}
+}
+
+class App extends Component {
+	render() {	
+  		return (
+  			<div className="App">
+  				<Subject title="WEB" sub="world wide web!"></Subject>
+  				<Subject title="React" sub="For UI"></Subject>
+  				<TOC></TOC>
+  				<Contents title="HTML" sub="HTML is HyperText Markup Language."></Contents>
+  			</div>
+  		);
+  	}
 }
 
 export default App;
