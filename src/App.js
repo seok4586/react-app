f82a742799e63423b73579186584c6e0ab9c5735
wandol <seok45861@gmail.com>
2019-10-03 16:58:17 +0900

    3주차소스
    이벤트

--- src/App.js
+++ src/App.js
@@ -6,13 +6,55 @@ import './App.css';
 
 
 class App extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			subject:{title:"web", sub:"world wide web!!"},
+			mode:"welcome",
+			select_contented_id:2,
+			welcome:{title:'welcome',desc:'Hello, react!!'},
+			contents:[
+				{id:1, title:'HTML', desc:'HTML is HyperText'},
+				{id:2, title:'CSS', desc:'CSS is for design'},
+				{id:3, title:'JavaScript', desc:'JavaScript is Hfor interactive'},
+			]
+		}
+	}
 	render() {	
-  		return (
+		var _title,_desc = null;
+		if(this.state.mode === 'welcome'){
+			_title = this.state.welcome.title;
+			_desc = this.state.welcome.desc;
+		}else if(this.state.mode === 'read'){
+			var i = 0;
+			while(i < this.state.contents.length){
+				var data = this.state.contents[i];
+				if(data.id === this.state.select_contented_id){
+					_title = data.title;
+					_desc = data.desc;	
+					break;
+				}
+				i = i + 1; 
+			}
+			
+		}
+  		 return ( 
   			<div className="App">
-  				<Subject title="WEB" sub="world wide web!"></Subject>
-  				<Subject title="React" sub="For UI"></Subject>
-  				<TOC></TOC>
-  				<Contents title="HTML" sub="HTML is HyperText Markup Language."></Contents>
+  				<Subject 
+  					title={this.state.subject.title} 
+  					sub={this.state.subject.sub}
+  					onChangPage={function(){
+  						this.setState({mode:'welcome'})
+  					}.bind(this)}
+  				>
+  				</Subject>
+  				<TOC onChangPage={function(id){
+					this.setState({
+						mode : 'read',
+						select_contented_id : Number(id)
+					});
+  				}.bind(this)} data={this.state.contents}></TOC>
+  				<Contents title={_title} sub={_desc}></Contents>
   			</div>
   		);
   	}
--- src/components/TOC.js
+++ src/components/TOC.js
@@ -2,14 +2,24 @@ import React, { Component } from 'react';
 
 
 class TOC extends Component {
-	render() {	
+	render() {
+		var lists = [];
+		var data = this.props.data;
+		var i = 0;
+		while(i<data.length){
+			lists.push(<li key={data[i].id}><a 
+				href={"/content/" + data[i].id} 
+				data-id={data[i].id}
+				onClick={function(id, e){
+					this.props.onChangPage(id); 
+					e.preventDefault();
+				}.bind(this,data[i].id)}>{data[i].title}</a></li>);
+			i = i + 1;
+		}	
   		return (
   			<nav>
 				<ul>
-				<li><a href="1.html">html</a></li>
-				<li><a href="2.html">css</a></li>
-				<li><a href="3.html">javascript</a></li>
-
+					{lists}
 				</ul>		
 			</nav>
   		);
